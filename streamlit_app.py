1e112732d8d3e4df2d1b02a241ddbd631bd20c58
Shashwat-Kush <124490948+Shashwat-Kush@users.noreply.github.com>
2024-01-18 15:21:09 +0530

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,15 +34,16 @@ def clear_chat_history():
 st.sidebar.button('Clear Chat History',on_click = clear_chat_history)
 
 #Function to generate Llama response
-def generate_llama_response(prompt):
-    string_dialogue = "You are a helpful assistant. You do not respond as user or pretend to be user.You only respond once as an assistant"
+def generate_llama_response(prompt_input):
+    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
     for dict_message in st.session_state.messages:
-        if dict_message['role']=='user':
-            string_dialogue +='User: ' +dict_message['content'] +"\n\n"
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
-            string_dialogue += 'Assistant:' + dict_message['content'] +'\n\n'
-        output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5',input = {'prompt':f"{string_dialogue}{prompt} Assistant:",'temperature':0.1,'top_p':0.9,'max_length':512,'repetition_penalty':1})
-        st.session_state.messages.append({'role':'assistant','content':output['generated_text']})
+            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
+                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
     return output
 
 #User Provided prompt
