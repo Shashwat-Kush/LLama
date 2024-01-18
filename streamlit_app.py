fd1c73113efde5244f98e588ee70587fdb910001
Shashwat-Kush <124490948+Shashwat-Kush@users.noreply.github.com>
2024-01-18 15:02:17 +0530

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ def generate_llama_response(prompt):
             string_dialogue +='User: ' +dict_message['content'] +"\n\n"
         else:
             string_dialogue += 'Assistant:' + dict_message['content'] +'\n\n'
-        output = replicate.run('',input = {'prompt':f"{string_dialogue}{prompt} Assistant:",'temperature':0.1,'top_p':0.9,'max_length':512,'repetition_penalty':1})
+        output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5',input = {'prompt':f"{string_dialogue}{prompt} Assistant:",'temperature':0.1,'top_p':0.9,'max_length':512,'repetition_penalty':1})
         st.session_state.messages.append({'role':'assistant','content':output['generated_text']})
     return output
 
